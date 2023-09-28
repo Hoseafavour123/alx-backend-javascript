63eb7c3615f49cd32afc5c1c6efd2776299bb5d4
Hosea Tonzai Favour <hoseafavour123@gmail.com>
2023-09-28 15:36:07 +0100

    Finished ES6 Classes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,8 @@
- import ClassRoom from './0-classroom';
+import ClassRoom from './0-classroom';
 
- export default function initializeRooms() {
-   const classOne = new ClassRoom(19);
-   const classTwo = new ClassRoom(20);
-   const classThree = new ClassRoom(34);
-   return [classOne, classTwo, classThree];
- }
+export default function initializeRooms() {
+  const classOne = new ClassRoom(19);
+  const classTwo = new ClassRoom(20);
+  const classThree = new ClassRoom(34);
+  return [classOne, classTwo, classThree];
+}
---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,41 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return self._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return self._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const clone = this.constructor[Symbol.species];
+
+    return new clone();
+  }
+}
